d301e0e8e1cc89ad086e53cdff494de67f5b853f
Sharanch <saranchmukhia@gmail.com>
2026-03-09 13:13:35 +0530

    add middleware and main.py

---
+++ app/middleware/logging_middleware.py
@@ -0,0 +1,14 @@
+import logging
+from starlette.middleware.base import BaseHTTPMiddleware
+
+
+
+class LoggingMiddlware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        logging.info(f"Request: {request.method} - {request.url}")
+
+        response = await call_next(request)
+
+        logging.info(f"Response: {response.status_code}")
+
+        return response
